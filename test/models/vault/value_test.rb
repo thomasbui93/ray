a3433c4a3669073a840b959ed8af5ecaf1f3bda4
Khoa Bui <thomasbui93@users.noreply.github.com>
2019-11-10 23:00:11 +0000

    Add basic test cases (#5)

--- test/models/vault/value_test.rb
+++ test/models/vault/value_test.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class Vault::ValueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'vault should have an adjacent list tree relation with itself' do
+    value = vault_values(:minimum_vault_parent)
+    child = vault_values(:minimum_vault_child)
+    assert Vault::Value.children(value.id).length, 1
+    assert Vault::Value.children(value.id).first, child
+  end
 end
